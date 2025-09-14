f279e94cc0702f132e1b479603cdf0c0b2d851c6
Nikhil Sagar Dadem <nikhildadem@gmail.com>
2025-09-14 05:35:59 -0700

    Initial commit with STM32F407 LIS3DSH demo files

---
+++ stm32f4_lis3dsh_spi_polling_demo/Debug/Drivers/Src/gpio_driver.d
@@ -0,0 +1,7 @@
+Drivers/Src/gpio_driver.o: ../Drivers/Src/gpio_driver.c \
+ C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h \
+ C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/gpio_driver.h \
+ C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h
+C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h:
+C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/gpio_driver.h:
+C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h:
---
+++ stm32f4_lis3dsh_spi_polling_demo/Debug/Src/main.d
@@ -0,0 +1,5 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h \
+ ../Inc/lis3dsh_spi_polling_demo.h
+C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h:
+../Inc/lis3dsh_spi_polling_demo.h:
