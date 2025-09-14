760364619bd36f8febd97baae0b9c57b8f7ba063
Nikhil Sagar Dadem <nikhildadem@gmail.com>
2025-09-14 05:25:27 -0700

    Clear repository contents

--- STM32f407VGT_Drivers/Debug/Drivers/Src/gpio_driver.d
+++
@@ -1,7 +0,0 @@
-Drivers/Src/gpio_driver.o: ../Drivers/Src/gpio_driver.c \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/gpio_driver.h \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h:
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/gpio_driver.h:
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h:
--- STM32f407VGT_Drivers/Debug/Src/msme.d
+++
@@ -1,9 +0,0 @@
-Src/msme.o: ../Src/msme.c \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/gpio_driver.h \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/spi_driver.h
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h:
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/gpio_driver.h:
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/STM32f407VGT.h:
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/STM32f407VGT_Drivers/Drivers/Inc/spi_driver.h:
