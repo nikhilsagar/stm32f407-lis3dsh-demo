04858297ba8b75c152032b8d64d617fbe9187d9d
Nikhil Sagar Dadem <nikhildadem@gmail.com>
2025-09-14 05:33:07 -0700

    Add .gitignore and stop tracking build artifacts

--- stm32f4_lis3dsh_spi_polling_demo/Debug/Drivers/Src/gpio_driver.d
+++
@@ -1,7 +0,0 @@
-Drivers/Src/gpio_driver.o: ../Drivers/Src/gpio_driver.c \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/gpio_driver.h \
- C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h:
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/gpio_driver.h:
-C:/Users/nikhi/Desktop/Embedded\ C/my_workspace/target/stm32f4_lis3dsh_spi_polling_demo/Drivers/Inc/STM32f407VGT.h:
